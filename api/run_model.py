073e0965ca43b27643db3904bd38cbb6d032ba70
Jooney <jhan756k@gmail.com>
2024-05-22 10:19:12 +0900

    t

--- api/run_model.py
+++ api/run_model.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter, BackgroundTasks
 from fastapi.responses import JSONResponse
 import pandas as pd
+import os 
 from tools import tdrefine, tdspectrogram
 from models.ai import model
 
 router = APIRouter()
+results = {}
 
 @router.post("/run_model/{file_location}")
 async def run_model(file_location: str, background_tasks: BackgroundTasks):
@@ -20,7 +22,11 @@ def process_file(file_location: str, dataframe: pd.DataFrame):
     try:
         tdrefine.refine(dataframe)
         tdspectrogram.spec(file_location)
-        model.predict(f"{file_location}.png")
-
+        prediction = model.predict(f"{file_location}.png")
+        results[file_location] = prediction.tolist()
+        
+        os.remove(file_location)
+        os.remove(f"{file_location}.png")
+        os.remove(f"{file_location}.csv")
     except Exception as e:
         print(f"Error processing file: {e}")
